4e5bd3bf335f9f42e769a805d4f2336c91d1285b
Martin <martin@hotmail.com>
2017-10-24 15:41:18 -0700

    Test commit

--- Wine Project.py
+++ Wine Project.py
@@ -1,43 +1,10 @@
+from Login import login
 import sqlite3
 conn = sqlite3.connect('LeVinEmployee.db')
 
 
-# Login
-
-def Login():
-
-    print("\n===============================================================================")
-    print("                          WELCOME TO LE VIN COUPE")
-    print("                Where wine and quality engineering collide!")
-    print("===============================================================================")
-
-
-    with conn:
-        cur = conn.cursor()
-
-        while True:
-            print("\nEnter your credentials below to login to the system.")
-            userEmail = input("\nPlease enter your email: ").strip()
-            userPassword = input("Please enter your password: ").strip()
-
-            if userEmail == "" or userPassword == "":
-                print("\nYou did not enter an email or password. Please try again.")
-            else:
-
-                try:
-                    cur.execute("SELECT * FROM Employee WHERE(Email = '" + userEmail + "') AND (Password = '" + userPassword + "')")
-
-                    results = cur.fetchall()
-
-                    if userEmail == results[0][7] and userPassword == results[0][8]:
-                        print("\nLogin successful")
-                        break
-                except:
-                    print("\nConnection Failed. You entered a wrong email or password. Please try again.")
-
 
 # Menu
-
 def Menu():
     print("\n===============================================================================")
     print("a. Register other employees")
@@ -208,7 +175,7 @@ def Registration():
 
 
 # Function calls:
-Login()
+login()
 Menu()
 # Menu option functions are called within the definition of the menu function.
 
