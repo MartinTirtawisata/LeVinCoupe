526d18e4489bcd79e7ca9dfd506a94c7e9244380
Martin <martin@hotmail.com>
2017-11-15 22:47:23 -0800

    registration

--- Tkinter_files/Tkinter_LeVinCoupe.py
+++ Tkinter_files/Tkinter_LeVinCoupe.py
@@ -9,6 +9,7 @@ import sqlite3
 def start_program():
     t_login()
 
+#---------------------LOGIN---------------------
 def t_login():
     global root_login
     global mainframe
@@ -64,6 +65,7 @@ def t_checkState():
     else:
         print("not checked")
 
+#--------------------------------------MENU---------------------------------
 def t_main_menu():
     global root_menu
     root_menu = Tk()
@@ -81,56 +83,103 @@ def t_main_menu():
     button_E.pack()
     root_menu.mainloop()
 
+#------------------------------------------------REGISTER---------------------------------------
 def t_check_register():
     root_menu.destroy()
     return t_register()
 
 def t_register():
+    global entry_emp_id
+    global entry_fname
+    global entry_lname
+    global entry_address
+    global entry_city
+    global entry_state
+    global entry_zip_code
+    global entry_email
+    global entry_password
     #Need Employee ID, First Name, Last Name,  Address, City, State, Zip Code, Email, Password
     root_register = Tk()
     root_register.title("LeVinCoupe Employee Register")
 
     label_emp_id = Label(root_register, text="Enter Employee ID")
-    label_fname = Label(root_register, text="Enter First Name")
-    label_lname = Label(root_register, text="Enter Last Name")
-    label_address = Label(root_register, text="Enter Address")
-    label_city = Label(root_register, text="Enter City")
-    label_state = Label(root_register, text="Enter State")
-    label_zip_code = Label(root_register, text="Enter Zip Code")
-    label_email = Label(root_register, text="Enter Email")
-    label_password = Label(root_register, text="Enter Password")
-    entry_emp_id = Entry(root_register)
-    entry_fname = Entry(root_register)
-    entry_lname = Entry(root_register)
-    entry_address = Entry(root_register)
-    entry_city = Entry(root_register)
-    entry_state = Entry(root_register)
-    entry_zip_code = Entry(root_register)
-    entry_email = Entry(root_register)
-    entry_password = Entry(root_register)
-
     label_emp_id.grid(row=0, column=0)
-    label_fname.grid(row=1, column=0)
-    label_lname.grid(row=2, column=0)
-    label_address.grid(row=3, column=0)
-    label_city.grid(row=4, column=0)
-    label_state.grid(row=5, column=0)
-    label_zip_code.grid(row=6, column=0)
-    label_email.grid(row=7, column=0)
-    label_password.grid(row=8, column=0)
+    entry_emp_id = Entry(root_register)
     entry_emp_id.grid(row=0, column=1)
+
+    label_fname = Label(root_register, text="Enter First Name")
+    label_fname.grid(row=1, column=0)
+    entry_fname = Entry(root_register)
     entry_fname.grid(row=1, column=1)
+
+    label_lname = Label(root_register, text="Enter Last Name")
+    label_lname.grid(row=2, column=0)
+    entry_lname = Entry(root_register)
     entry_lname.grid(row=2, column=1)
+
+    label_address = Label(root_register, text="Enter Address")
+    label_address.grid(row=3, column=0)
+    entry_address = Entry(root_register)
     entry_address.grid(row=3, column=1)
+
+    label_city = Label(root_register, text="Enter City")
+    label_city.grid(row=4, column=0)
+    entry_city = Entry(root_register)
     entry_city.grid(row=4, column=1)
+
+    label_state = Label(root_register, text="Enter State")
+    label_state.grid(row=5, column=0)
+    entry_state = Entry(root_register)
     entry_state.grid(row=5, column=1)
+
+    label_zip_code = Label(root_register, text="Enter Zip Code")
+    label_zip_code.grid(row=6, column=0)
+    entry_zip_code = Entry(root_register)
     entry_zip_code.grid(row=6, column=1)
+
+    label_email = Label(root_register, text="Enter Email")
+    label_email.grid(row=7, column=0)
+    entry_email = Entry(root_register)
     entry_email.grid(row=7, column=1)
+
+    label_password = Label(root_register, text="Enter Password")
+    label_password.grid(row=8, column=0)
+    entry_password = Entry(root_register)
     entry_password.grid(row=8, column=1)
 
-    button_sign_up = Button(root_register, text="Sign Up")
+    button_sign_up = Button(root_register, text="Sign Up", command=t_check_registration)
     button_sign_up.grid(columnspan=3)
     root_register.mainloop()
+
+def t_check_registration():
+
+    userEmp_id = entry_emp_id.get()
+    userF_name = str(entry_fname.get())
+    userL_name = str(entry_lname.get())
+    userAddress = str(entry_address.get())
+    userCity = str(entry_city.get())
+    userState = str(entry_state.get())
+    userZip_code = str(entry_zip_code.get())
+    userEmail = str(entry_email.get())
+    userPassword = str(entry_password.get())
+
+    try:
+        with sqlite3.connect('LeVinEmployee.db') as conn:
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO Employee VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                            (userEmp_id, userF_name, userL_name, userAddress, userCity, userState, userZip_code, userEmail, userPassword))
+            query_result = cursor.fetchall()
+            print(query_result)
+    except sqlite3.Error as e:
+        print(e)
+
+
+
+
+
+
+
+
 
 def t_check_association():
     root_menu.destroy()
