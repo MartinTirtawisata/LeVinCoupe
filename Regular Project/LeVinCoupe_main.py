1e8a800609e327e50a237a584843c4200e6c7e45
SeanScud <seanscudellari@gmail.com>
2017-11-21 00:34:25 -0800

    update

--- Regular Project/LeVinCoupe_main.py
+++ Regular Project/LeVinCoupe_main.py
@@ -12,20 +12,6 @@ from Registration import registration
 from Menu import menu
 conn = sqlite3.connect('LeVinEmployee.db')
 
-'''
-Additional Features:
-1. Association test with ability for user to input the two desired characteristics (option d in our menu).
-2. Printed min and max value for user chosen characteristic in frequency distribution and prompted a user to enter a value within the range (including the lower and upper limits).
-3. Alerting users to inputted values that are more than 2 SDs from the mean for their chosen characteristic for frequency distribution.
-                        -Note that the graph will still be displayed if user enters a value more than 2 SDs away from the mean for their chosen characteristic.
-                        -Min/Max and Mean/SD values for all wine characteristics are listed at the top of the Frequency_Distribution file.
-4. Blending recommendations for the frequency distribution results:
-                            -Issued a blending recommendation if user enters a value for volatile acidity above the regulated amount by the federal Tax and Trade Bureau for red and white wines. Note that the distribution will still be displayed.
-                            -Issued a blending recommendation if user enters a value for residual sugar greater than or equal to 20.
-                            -Issued a blending recommendation if user enters a value for residual sugar below 4.
-                            -Issued a blending recommendation if user enters a value for fixed acidity greater than or equal to 12.
-5. GUI
-'''
 
 #-------------------------------------------Beginning of Code---------------------------------------------
 print("\n===============================================================================")
